5ba725788bb1c691ada94291a2b62e02e7d5ec38
unknown-user <unknown-user@fe80::6482:6863:6fca:e6a5%8>
2015-07-21 17:13:24 +0800

    Check in MERP V2 Production

---
+++ merp/Activities/InvoiceActivity.cs
@@ -0,0 +1,322 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Android.Bluetooth;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System.IO;
+using SQLite;
+using System.Threading;
+using Java.Util;
+
+namespace wincom.mobile.erp
+{
+	[Activity (Label = "INVOICE")]			
+	public class InvoiceActivity : Activity
+	{
+		ListView listView ;
+		List<Invoice> listData = new List<Invoice> ();
+		string pathToDatabase;
+		BluetoothAdapter mBluetoothAdapter;
+		BluetoothSocket mmSocket;
+		BluetoothDevice mmDevice;
+		//Thread workerThread;
+		Stream mmOutputStream;
+		AdPara apara=null;
+		CompanyInfo compinfo;
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+			if (!((GlobalvarsApp)this.Application).ISLOGON) {
+				Finish ();
+			}
+			// Create your application here
+			SetContentView (Resource.Layout.ListView);
+			pathToDatabase = ((GlobalvarsApp)this.Application).DATABASE_PATH;
+			populate (listData);
+			apara =  DataHelper.GetAdPara (pathToDatabase);
+			listView = FindViewById<ListView> (Resource.Id.feedList);
+			Button butNew= FindViewById<Button> (Resource.Id.butnewInv); 
+			butNew.Click += butCreateNewInv;
+			Button butInvBack= FindViewById<Button> (Resource.Id.butInvBack); 
+			butInvBack.Click += (object sender, EventArgs e) => {
+				StartActivity(typeof(MainActivity));
+			};
+
+			listView.ItemClick += OnListItemClick;
+			listView.ItemLongClick += OnListItemLongClick;
+			//listView.Adapter = new CusotmListAdapter(this, listData);
+			SetViewDlg viewdlg = SetViewDelegate;
+			listView.Adapter = new GenericListAdapter<Invoice> (this, listData, Resource.Layout.ListItemRow, viewdlg);
+
+		}
+
+		public override void OnBackPressed() {
+			// do nothing.
+		}
+
+		private void SetViewDelegate(View view,object clsobj)
+		{
+			Invoice item = (Invoice)clsobj;
+			view.FindViewById<TextView> (Resource.Id.invdate).Text = item.invdate.ToString ("dd-MM-yy");
+			view.FindViewById<TextView> (Resource.Id.invno).Text = item.invno;
+			view.FindViewById<TextView> (Resource.Id.trxtype).Text = item.trxtype;
+			view.FindViewById<TextView>(Resource.Id.invcust).Text = item.description;
+			//view.FindViewById<TextView> (Resource.Id.Amount).Text = item.amount.ToString("n2");
+			view.FindViewById<TextView> (Resource.Id.TaxAmount).Text = item.taxamt.ToString("n2");
+			double ttl = item.amount + item.taxamt;
+			view.FindViewById<TextView> (Resource.Id.TtlAmount).Text =ttl.ToString("n2");
+			ImageView img = view.FindViewById<ImageView> (Resource.Id.printed);
+			if (!item.isPrinted)
+				img.Visibility = ViewStates.Invisible;
+		}
+
+		protected override void OnResume()
+		{
+			base.OnResume();
+			listData = new List<Invoice> ();
+			populate (listData);
+			apara =  DataHelper.GetAdPara (pathToDatabase);
+			listView = FindViewById<ListView> (Resource.Id.feedList);
+			SetViewDlg viewdlg = SetViewDelegate;
+			listView.Adapter = new GenericListAdapter<Invoice> (this, listData, Resource.Layout.ListItemRow, viewdlg);
+		}
+
+		void OnListItemClick(object sender, AdapterView.ItemClickEventArgs e) {
+			Invoice item = listData.ElementAt (e.Position);
+			var intent = new Intent(this, typeof(InvItemActivity));
+			intent.PutExtra ("invoiceno",item.invno );
+			intent.PutExtra ("custcode",item.custcode );
+			StartActivity(intent);
+		}
+
+		void OnListItemLongClick(object sender, AdapterView.ItemLongClickEventArgs e) {
+			Invoice item = listData.ElementAt (e.Position);
+			PopupMenu menu = new PopupMenu (e.Parent.Context, e.View);
+			menu.Inflate (Resource.Menu.popupInv);
+
+			if (!compinfo.AllowDelete) {
+				menu.Menu.RemoveItem (Resource.Id.popInvdelete);
+			}
+			if (DataHelper.GetInvoicePrintStatus (pathToDatabase, item.invno)) {
+				menu.Menu.RemoveItem (Resource.Id.popInvdelete);
+			}
+			menu.MenuItemClick += (s1, arg1) => {
+				if (arg1.Item.TitleFormatted.ToString().ToLower()=="add")
+				{
+					CreateNewInvoice();
+				}else if (arg1.Item.TitleFormatted.ToString().ToLower()=="print")
+				{
+					PrintInv(item,1);	
+				}else if (arg1.Item.TitleFormatted.ToString().ToLower()=="print 2 copy")
+				{
+					PrintInv(item,2);	
+
+				} else if (arg1.Item.TitleFormatted.ToString().ToLower()=="delete")
+				{
+					Delete(item);
+				}
+
+			};
+			menu.Show ();
+		}
+
+		void Delete(Invoice inv)
+		{
+			var builder = new AlertDialog.Builder(this);
+			builder.SetMessage("Confimr to Delete?");
+			builder.SetPositiveButton("YES", (s, e) => { DeleteItem(inv); });
+			builder.SetNegativeButton("Cancel", (s, e) => { /* do something on Cancel click */ });
+			builder.Create().Show();
+		}
+		void DeleteItem(Invoice inv)
+		{
+			using (var db = new SQLite.SQLiteConnection(pathToDatabase))
+			{
+				var list = db.Table<Invoice>().Where(x=>x.invno==inv.invno).ToList<Invoice>();
+				if (list.Count > 0) {
+					db.Delete (list [0]);
+					var arrlist= listData.Where(x=>x.invno==inv.invno).ToList<Invoice>();
+					if (arrlist.Count > 0) {
+						listData.Remove (arrlist [0]);
+						SetViewDlg viewdlg = SetViewDelegate;
+						listView.Adapter = new GenericListAdapter<Invoice> (this, listData, Resource.Layout.ListItemRow, viewdlg);
+					}
+				}
+			}
+		}
+
+		void populate(List<Invoice> list)
+		{
+			using (var db = new SQLite.SQLiteConnection(pathToDatabase))
+			{
+				var list2 = db.Table<Invoice>().ToList<Invoice>().Where(x=>x.isUploaded==false);
+				foreach(var item in list2)
+				{
+					list.Add(item);
+				}
+
+			}
+			compinfo = DataHelper.GetCompany (pathToDatabase);
+		}
+
+
+		private void butCreateNewInv(object sender, EventArgs e)
+		{
+			CreateNewInvoice ();
+		}
+
+		private void CreateNewInvoice()
+		{
+			var intent = new Intent(this, typeof(CreateInvoice));
+			StartActivity(intent);
+		}
+
+//		void PrintInvSumm(DateTime printdate1,DateTime printdate2)
+//		{
+//			mmDevice = null;
+//			findBTPrinter ();
+//
+//			if (mmDevice == null)
+//				return;
+//			
+//			string userid = ((GlobalvarsApp)this.Application).USERID_CODE;
+//			PrintInvHelper prnHelp = new PrintInvHelper (pathToDatabase, userid);
+//			string msg = prnHelp.PrintInvSumm(mmSocket, mmDevice,printdate1,printdate2);
+//			Toast.MakeText (this, msg, ToastLength.Long).Show ();	
+//
+//		}
+		void PrintInv(Invoice inv,int noofcopy)
+		{
+			//Toast.MakeText (this, "print....", ToastLength.Long).Show ();	
+			InvoiceDtls[] list;
+			using (var db = new SQLite.SQLiteConnection (pathToDatabase)){
+				var ls= db.Table<InvoiceDtls> ().Where (x => x.invno==inv.invno).ToList<InvoiceDtls>();
+				list = new InvoiceDtls[ls.Count];
+				ls.CopyTo (list);
+			}
+			mmDevice = null;
+			findBTPrinter ();
+
+			if (mmDevice != null) {
+				StartPrint (inv, list,noofcopy);
+				updatePrintedStatus (inv);
+			}
+		
+		}
+
+		void updatePrintedStatus(Invoice inv)
+		{
+			using (var db = new SQLite.SQLiteConnection (pathToDatabase)) {
+				var list = db.Table<Invoice> ().Where (x => x.invno == inv.invno).ToList<Invoice> ();
+				if (list.Count > 0) {
+					//if only contains items then allow to update the printed status.
+					//this to allow the invoice;s item can be added. if not can not be posted(upload)
+					var list2 = db.Table<InvoiceDtls> ().Where (x => x.invno == inv.invno).ToList<InvoiceDtls> ();
+					if (list2.Count > 0) {
+						list [0].isPrinted = true;
+						db.Update (list [0]);
+					}
+				}
+			}
+		}
+
+		void StartPrint(Invoice inv,InvoiceDtls[] list,int noofcopy )
+		{
+			string userid = ((GlobalvarsApp)this.Application).USERID_CODE;
+			PrintInvHelper prnHelp = new PrintInvHelper (pathToDatabase, userid);
+			string msg =prnHelp.OpenBTAndPrint (mmSocket, mmDevice, inv, list,noofcopy);
+			Toast.MakeText (this, msg, ToastLength.Long).Show ();	
+			//AlertShow (msg);
+		}
+
+		string getBTAddrFile(string printername)
+		{
+			var documents = System.Environment.GetFolderPath (System.Environment.SpecialFolder.Personal);
+			string filename = Path.Combine (documents, printername+".baddr");
+			return filename;
+		}
+
+		bool tryConnectBtAddr(string btAddrfile)
+		{
+			bool found = false;
+			if (!File.Exists (btAddrfile))
+				return false;
+			try{
+			mBluetoothAdapter = BluetoothAdapter.DefaultAdapter;
+			mmDevice = mBluetoothAdapter.GetRemoteDevice (File.ReadAllBytes (btAddrfile));
+			if (mmDevice != null) {
+				found = true;
+			}
+			
+			}catch(Exception ex) {
+			
+			}
+			return found;
+	     }
+
+		void AlertShow(string text)
+		{
+			AlertDialog.Builder alert = new AlertDialog.Builder (this);
+
+			alert.SetMessage (text);
+			RunOnUiThread (() => {
+				alert.Show();
+			} );
+			
+		}
+		void findBTPrinter(){
+			string printername = apara.PrinterName.Trim ().ToUpper ();
+//			string addrfile = getBTAddrFile(printername);
+//			if (tryConnectBtAddr(addrfile))
+//				return;
+			
+			try{
+				mBluetoothAdapter = BluetoothAdapter.DefaultAdapter;
+
+				if (mBluetoothAdapter ==null)
+				{
+					Toast.MakeText (this, "Can not Find Bluetooth device,try again.", ToastLength.Long).Show ();	
+					return;
+				}
+				string txt ="";
+				if (!mBluetoothAdapter.Enable()) {
+					Intent enableBluetooth = new Intent(
+						BluetoothAdapter.ActionRequestEnable);
+					StartActivityForResult(enableBluetooth, 0);
+				}
+
+				var pair= mBluetoothAdapter.BondedDevices;
+				if (pair.Count > 0) {
+					foreach (BluetoothDevice dev in pair) {
+						Console.WriteLine (dev.Name);
+						txt = txt+","+dev.Name;
+						if (dev.Name.ToUpper()==printername)
+						{
+							mmDevice = dev;
+//							File.WriteAllText(addrfile,dev.Address);
+							break;
+						}
+					}
+				}
+				Toast.MakeText(this, "found device " +mmDevice.Name, ToastLength.Long).Show ();	
+				//AlertShow( "found device " +mmDevice.Name);
+				//txtv.Text ="found device " +mmDevice.Name;
+			}catch(Exception ex) {
+				//txtv.Text = ex.Message;
+				mmDevice = null;
+				Toast.MakeText (this, "Error in Bluetooth device. Try again.", ToastLength.Long).Show ();	
+				//AlertShow(ex.Message);
+			}
+		}
+
+
+	}
+}
+
---
+++ merp/Activities/MainActivity.cs
@@ -0,0 +1,401 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using SQLite;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using WcfServiceItem;
+using Android.Telephony;
+using Android.Accounts;
+using Android.Text;
+using System.Net;
+
+namespace wincom.mobile.erp
+{
+	[Activity (Label = "WINCOM M-ERP",Icon = "@drawable/icon")]
+	public class MainActivity :Activity
+	{
+		//List<Item> items = null;
+
+		private Service1Client _client;
+		string pathToDatabase;
+		string COMPCODE;
+		string BRANCODE;
+		string USERID;
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+			if (!((GlobalvarsApp)this.Application).ISLOGON) {
+				Finish ();
+				return;
+			}
+			// Set our view from the "main" layout resource
+			SetContentView (Resource.Layout.Main);
+			GetDBPath ();
+
+			AndroidEnvironment.UnhandledExceptionRaiser += AndroidEnvironment_UnhandledExceptionRaiser;
+			Button but = FindViewById<Button> (Resource.Id.butSecond);
+			but.Click += butClick;
+			Button butInvlist = FindViewById<Button> (Resource.Id.butInvlist);
+			butInvlist.Click+= ButInvlist_Click;
+			Button butdown = FindViewById<Button> (Resource.Id.butDown);
+			butdown.Click += butDownloadItems;
+			Button butMItem = FindViewById<Button> (Resource.Id.butMaster);
+			butMItem.Click += butMasterClick;
+			Button butSett = FindViewById<Button> (Resource.Id.butsetting);
+			butSett.Click += butSetting;
+//			Button butCustProf = FindViewById<Button> (Resource.Id.butCustProf);
+//			butCustProf.Click+=  butCustomerClick;
+			Button butlogOff = FindViewById<Button> (Resource.Id.butOut);
+			butlogOff.Click += ButlogOff_Click;
+			Button butAbt = FindViewById<Button> (Resource.Id.butAbout);
+			butAbt.Click+= ButAbt_Click;
+//			Button butupload = FindViewById<Button> (Resource.Id.butupload);
+//			butupload.Click += butUploadBills;
+			//Button butExitOnly = FindViewById<Button> (Resource.Id.butExitOnly);
+//			Button butsumm = FindViewById<Button> (Resource.Id.butInvsumm);
+//			butsumm.Click+= (object sender, EventArgs e) => {
+//				StartActivity(typeof(PrintSumm));
+//			};
+
+			Button butCNNote = FindViewById<Button> (Resource.Id.butcnnote);
+			butCNNote.Click+= ButCNNote_Click;
+//			Button butCNNoteList = FindViewById<Button> (Resource.Id.butCNlist);
+//			butCNNoteList.Click+= ButCNNoteList_Click;
+		}
+
+		void ButCNNoteList_Click (object sender, EventArgs e)
+		{
+			var intent = new Intent(this, typeof(CNAllActivity));
+
+			StartActivity(intent);
+		}
+
+		void ButCNNote_Click (object sender, EventArgs e)
+		{
+			var intent = new Intent(this, typeof(CNNoteActivity));
+
+			StartActivity(intent);
+		}
+
+		void ButAbt_Click (object sender, EventArgs e)
+		{
+			CompanyInfo comp= DataHelper.GetCompany (pathToDatabase);
+			View messageView = LayoutInflater.Inflate(Resource.Layout.About, null, false);
+
+			// When linking text, force to always use default color. This works
+			// around a pressed color state bug.
+			TextView textView = (TextView) messageView.FindViewById(Resource.Id.about_credits);
+			TextView textDesc = (TextView) messageView.FindViewById(Resource.Id.about_descrip);
+			//textDesc.Text = Html.FromHtml (Resources.GetString(Resource.String.app_descrip))..ToString();
+			textView.Text = "For inquiry, please contact " + comp.SupportContat;
+
+			AlertDialog.Builder builder = new AlertDialog.Builder(this);
+			builder.SetIcon(Resource.Drawable.Icon);
+			builder.SetTitle(Resource.String.app_name);
+			builder.SetView(messageView);
+			builder.Create();
+			builder.Show();
+		}
+
+		void AndroidEnvironment_UnhandledExceptionRaiser (object sender, RaiseThrowableEventArgs e)
+		{
+			Toast.MakeText (this, e.Exception.Message, ToastLength.Long).Show ();	
+			StartActivity (typeof(MainActivity));
+			Finish ();
+		}
+
+		protected override void Dispose(bool disposing)
+		{
+			AndroidEnvironment.UnhandledExceptionRaiser -=AndroidEnvironment_UnhandledExceptionRaiser;
+			base.Dispose(disposing);
+		}
+
+		void ButInvlist_Click (object sender, EventArgs e)
+		{
+			var intent = new Intent(this, typeof(TransListActivity));
+
+			StartActivity(intent);
+		}
+
+		public override bool  OnCreateOptionsMenu(IMenu menu)
+		{
+			MenuInflater.Inflate(Resource.Menu.MainMenu,menu);
+			return base.OnPrepareOptionsMenu(menu);
+		}
+
+		public override bool OnOptionsItemSelected(IMenuItem item)
+		{
+			switch (item.ItemId)
+			{
+			case Resource.Id.mmenu_back:
+				BackupDatabase ();
+				return true;
+//			case Resource.Id.mmenu_Reset:
+//				//do something
+//				return true;
+			case Resource.Id.mmenu_setting:
+				StartActivity (typeof(SettingActivity));
+				return true;
+			case Resource.Id.mmenu_logoff:
+				RunOnUiThread(() =>ExitAndLogOff()) ;
+				return true;
+			case Resource.Id.mmenu_downcompinfo:
+				DownloadCompInfo ();
+				return true;
+			case Resource.Id.mmenu_clear:
+				var builder = new AlertDialog.Builder(this);
+				builder.SetMessage("Confirm to CLEAR ? All transaction will be deleted. Deleted records are not recoverable.");
+				builder.SetPositiveButton("OK", (s, e) => { ClearPostedInv () ;});
+				builder.SetNegativeButton("Cancel", (s, e) => { /* do something on Cancel click */ });
+				builder.Create().Show();
+
+				return true;
+			}
+		
+			return base.OnOptionsItemSelected(item);
+		}
+
+		void GetDBPath ()
+		{
+			COMPCODE = ((GlobalvarsApp)this.Application).COMPANY_CODE;
+			BRANCODE = ((GlobalvarsApp)this.Application).BRANCH_CODE;
+			USERID = ((GlobalvarsApp)this.Application).USERID_CODE;
+			pathToDatabase = ((GlobalvarsApp)this.Application).DATABASE_PATH;
+		}
+
+		protected override void OnResume()
+		{
+			base.OnResume();
+			GetDBPath ();
+		}
+
+		void ClearPostedInv()
+		{
+			CompanyInfo para = DataHelper.GetCompany (pathToDatabase);
+			if (!para.AllowClrTrxHis) {
+				Toast.MakeText (this, "Access denied...", ToastLength.Long).Show ();	
+				return;
+			}
+			using (var db = new SQLite.SQLiteConnection(pathToDatabase))
+			{
+				db.DeleteAll<InvoiceDtls> ();
+				db.DeleteAll<Invoice> ();
+				db.DeleteAll<CNNoteDtls> ();
+				db.DeleteAll<CNNote> ();
+//				var list2 = db.Table<Invoice> ().Where (x => x.isUploaded == true).ToList<Invoice> ();
+//				db.RunInTransaction (() => {
+//					foreach (var item in list2) {
+//						var listitm = db.Table<InvoiceDtls> ().Where (x => x.invno == item.invno).ToList<InvoiceDtls> ();
+//						foreach(var iitem in listitm){
+//							db.Delete(iitem);
+//						}
+//						db.Delete(item);
+//					}
+//				});
+//
+//				var list3 = db.Table<CNNote> ().Where (x => x.isUploaded == true).ToList<CNNote> ();
+//				db.RunInTransaction (() => {
+//					foreach (var item in list3) {
+//						var listitm = db.Table<CNNoteDtls> ().Where (x => x.cnno == item.cnno).ToList<CNNoteDtls> ();
+//						foreach(var iitem in listitm){
+//							db.Delete(iitem);
+//						}
+//						db.Delete(item);
+//					}
+//				});
+
+				Toast.MakeText (this, "Transaction clear...", ToastLength.Long).Show ();	
+			}
+		}
+
+		void butSysSetting(object sender,EventArgs e)
+		{
+			StartActivity (typeof(SettingActivity));
+		}
+
+		void BackupDatabase ()
+		{
+			var sdcard = Path.Combine (Android.OS.Environment.ExternalStorageDirectory.Path, "erpdata");
+			if (!Directory.Exists (sdcard)) {
+				Directory.CreateDirectory (sdcard);
+			}
+			string filename = Path.Combine (sdcard,"erplite"+ DateTime.Now.ToString("yyMMddHHmm") +".db");
+			if (File.Exists (pathToDatabase)) {
+				File.Copy (pathToDatabase, filename, true);
+				UploadToErpHostForSupport (filename);
+			}
+		}
+
+		private void UploadToErpHostForSupport(string filename)
+		{
+			WebClient myWebClient = new WebClient();
+			try{
+				
+			myWebClient.QueryString["COMP"] = COMPCODE;
+			myWebClient.QueryString["BRAN"] = BRANCODE;
+			myWebClient.QueryString["USER"] = USERID;
+				byte[] responseArray = myWebClient.UploadFile(@"http://www.wincomcloud.com/UploadDb/uploadDb.aspx",filename);
+			//Console.WriteLine("\nResponse Received.The contents of the file uploaded are:\n{0}",
+			//	System.Text.Encoding.ASCII.GetString(responseArray));
+			}catch {
+			
+			}
+		}
+
+		void butBackUpDb(object sender,EventArgs e)
+		{
+			BackupDatabase ();
+		}
+
+		void butUploadBills(object sender,EventArgs e)
+		{
+			Button butupload =  FindViewById<Button> (Resource.Id.butupload);
+			butupload.Enabled = false;
+			//UploadBillsToServer();
+			UploadHelper upload= new UploadHelper();
+			upload.Uploadhandle = OnUploadDoneDlg; 
+			upload.CallingActivity = this;
+			upload.startUpload ();		
+		}
+
+		void butDownloadItems(object sender,EventArgs e)
+		{
+			StartActivity (typeof(DownloadActivity));
+//			Button butDown =  FindViewById<Button> (Resource.Id.butDown);
+//			butDown.Enabled = false;
+//			DownloadHelper download= new DownloadHelper();
+//			download.Downloadhandle = DownItemsDoneDlg; 
+//			download.CallingActivity = this;
+//			download.startDownloadItem ();
+		}
+		void butSetting(object sender,EventArgs e)
+		{
+			StartActivity (typeof(SettingActivity));
+		}
+//		void butDownloadCusts(object sender,EventArgs e)
+//		{
+//			Button butDown =  FindViewById<Button> (Resource.Id.butDownCust);
+//			butDown.Enabled = false;
+//			DownloadHelper download= new DownloadHelper();
+//			download.Downloadhandle =  DownCustDoneDlg; 
+//			download.CallingActivity = this;
+//			download.startDownloadCustomer ();
+//		}
+
+		private void DownloadCompInfo()
+		{
+			DownloadHelper download= new DownloadHelper();
+			download.Downloadhandle = OnDownProfileDoneDlg; 
+			download.CallingActivity = this;
+			download.startDownloadCompInfo();
+		}
+
+		private void OnDownProfileDoneDlg(Activity callingAct,int count,string msg)
+		{
+			Toast.MakeText (this, msg, ToastLength.Long).Show ();
+		}
+
+		private void OnUploadDoneDlg(Activity callingAct,int count,string msg)
+		{
+			Button butupload = callingAct.FindViewById<Button> (Resource.Id.butupload);
+			butupload.Enabled = true;
+			if (count > 0) {
+				string dispmsg = "Total " + count.ToString () + " invoices uploaded.";
+				Toast.MakeText (this, dispmsg, ToastLength.Long).Show ();	
+			} else {
+				Toast.MakeText (this, msg, ToastLength.Long).Show ();	
+			}
+		}
+
+		private void DownItemsDoneDlg(Activity callingAct,int count,string msg)
+		{
+			Button butdown = FindViewById<Button> (Resource.Id.butDown);
+			butdown.Enabled = true;
+			if (count > 0) {
+				string dispmsg = "Total " + count.ToString () + " Items downloaded.";
+				Toast.MakeText (this, dispmsg, ToastLength.Long).Show ();	
+			} else {
+				Toast.MakeText (this, msg, ToastLength.Long).Show ();	
+			}
+		}
+
+		private void DownCustDoneDlg(Activity callingAct,int count,string msg)
+		{
+			Button butdown = FindViewById<Button> (Resource.Id.butDownCust);
+			butdown.Enabled = true;
+			if (count > 0) {
+				string dispmsg = "Total " + count.ToString () + " Customers downloaded.";
+				Toast.MakeText (this, dispmsg, ToastLength.Long).Show ();	
+			} else {
+				Toast.MakeText (this, msg, ToastLength.Long).Show ();	
+			}
+		}
+
+		void ExitApp ()
+		{
+			//var intent = new Intent (this, typeof(LoginActivity));
+			//StartActivity (intent);
+			((GlobalvarsApp)this.Application).ISLOGON = false;
+			Finish ();
+			Android.OS.Process.KillProcess (Android.OS.Process.MyPid ());
+			Parent.Finish ();
+			Intent intent = new Intent(Intent.ActionMain);
+ 			intent.AddCategory(Intent.CategoryHome);
+			intent.SetFlags(ActivityFlags.NewTask);
+			StartActivity(intent);
+
+		}
+
+		void ExitAndLogOff ()
+		{
+			using (var db = new SQLite.SQLiteConnection (pathToDatabase)) {
+				var list2 = db.Table<AdUser> ().ToList<AdUser> ();
+				if (list2.Count > 0) {
+					list2 [0].Islogon = false;
+					db.Update (list2 [0], typeof(AdUser));
+				}
+			}
+			RunOnUiThread (() => ExitApp ());
+		}
+
+		void ButlogOff_Click (object sender, EventArgs e)
+		{
+			RunOnUiThread (() => ExitApp ());
+
+		}
+
+		private void butClick(object sender,EventArgs e)
+		{
+			var intent = new Intent(this, typeof(InvoiceActivity));
+
+			StartActivity(intent);
+		}
+
+		private void butMasterClick(object sender,EventArgs e)
+		{
+			var intent = new Intent(this, typeof(MasterRefActivity));
+
+			StartActivity(intent);
+		}
+		private void butCustomerClick(object sender,EventArgs e)
+		{
+			var intent = new Intent(this, typeof(CustomerActivity));
+
+			StartActivity(intent);
+		}
+
+		public override void OnBackPressed() {
+			// do nothing.
+		}
+
+	}
+}
+
+
